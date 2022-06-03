86f9ccd154c3b6efa7c40f76a8e12b76b990a2ae
Raj Gedhada <grajnikanth@gmail.com>
2022-06-03 15:57:57 -0700

    Reading and writing simplified using BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Async chat server
 use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 
 // Building an echo server which receives the clients message and sends it back to client
 
@@ -24,26 +24,44 @@ async fn main() {
         // addr will not be used so to eliminate unused errors we use "_" in front
     let (mut socket, _addr) = listener.accept().await.unwrap();
 
+    // instead of using socket.read() we will use a BufReader which can read lines as 
+    // byte streams. This will eliminate us haveing to deal with the number of bytes
+    // we have to store in our buffer etc.
+
+    // first we need to split the socket to distinguish between message receiver and
+    // message sender to client
+    // 
+    let (socket_reader, mut socket_writer) = socket.split();
+
+    // BufReader will do the heavy lifting of tracking number of bytes read etc. 
+    // We only have to worry about reading one line at a time
+    let mut reader = BufReader::new(socket_reader);
+
+    // Each line read will be stored in the variable line
+    let mut line = String::new();
+
     //single message can be received and echoed back to the client with the following code
     // You can put the below code into an infinite loop to receive and echo multiple
     // messages
 
-    // loop {
-    //     // make a buffer which can hold upto 1024 bytes or 1KB
-    //     let mut buffer = [0u8; 1024];
+    loop {
+        // The read_line implemented as a trait on AsyncBufReadExt
 
-    //     // read the bytes in the socket and store it in the buffer. 
-    //     // bytes_read will store how many actual bytes were read by the socket
-    //     let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let bytes_read = reader.read_line(&mut line).await.unwrap();  
 
-    //     // take all the bytes and write it back to the socket 
-    //     // with the ..bytes_read we are saying send the bytes truncated by the 
-    //     // the number of bytes correspoding to bytes_read
-    //     socket.write_all(&buffer[..bytes_read]).await.unwrap();
-    //     // after writing the message back there is nothing else and the connection is stopped
+        // if no new lines are entered we break out of the loop
+        // The below happens when the client exits the connection
+        if bytes_read == 0 {
+            break;
+        }
 
-    // }
-    
-    let mut reader = 10;
+        socket_writer.write_all(line.as_bytes()).await.unwrap();
+
+        // clear the contents stored. As the read_line function appends to the contents
+        // of the line variable. We want to clear it so that each new line is echoed back
+        // rather than all the lines entered so far
+        line.clear();
+
+    }
 
 }
